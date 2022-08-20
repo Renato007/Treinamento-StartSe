f66e37920d009276583284229ec267e60b3be676
Renato Soares <renatosilsoa@gmail.com>
2022-08-20 10:46:39 -0300

    aula 01 js -função que muda o estilo  de cor da página

--- codigos/aula-js/js_externo.js
+++ codigos/aula-js/js_externo.js
@@ -1,11 +1,23 @@
-document.querySelector("#titulo").textContent = "aula de javascript -Introdução";
-document.querySelector("#sub_titulo").textContent ="Bora pra cima"
+document.querySelector("#titulo").textContent =
+  "aula de javascript -Introdução";
+document.querySelector("#sub_titulo").textContent = "Bora pra cima";
 
+//Apresentar uma msg e esconde o form de login
 
-//Apresentar uma msg e esconde o form de login 
-function bemVindo(){
-    const nome = document.querySelector("#idNomeUsuario").value;
-    alert(`Olá ${nome}! Sejá bem-vindo!`);
-    document.getElementById('formularioLogin').style.display ='none';
+/* function bemVindo() {
+  const nome = document.querySelector("#idNomeUsuario").value;
+  alert(`Olá ${nome}! Sejá bem-vindo!`);
+  document.getElementById("formularioLogin").style.display = "none";
+} */
+
+//Permite mudar a cor de fundo da página 
+function mudaCorFundo() {
+  const menu = getSelection("#coresFundo");
+
+  const cor = menu.options[menu.selectedIndex].value;
+
+  document.body.style.background = cor;
+  console.log(cor);
+  console.log(menu);
 }
 
