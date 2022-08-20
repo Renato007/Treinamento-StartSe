02b1c54b9162026f5f917375ea2acd1d23788aa4
Renato Soares <renatosilsoa@gmail.com>
2022-08-20 10:25:35 -0300

    aula02 - ex - função que apresenta msg e esconde elemento html

--- codigos/aula-js/js_externo.js
+++ codigos/aula-js/js_externo.js
@@ -1,6 +1,11 @@
 document.querySelector("#titulo").textContent = "aula de javascript -Introdução";
 document.querySelector("#sub_titulo").textContent ="Bora pra cima"
 
-const  nome = prompt("Qual é o seu nome");
 
-alert (`Que bom ter você aqui ${nome}! \n Vamos Começar?`);
+//Apresentar uma msg e esconde o form de login 
+function bemVindo(){
+    const nome = document.querySelector("#idNomeUsuario").value;
+    alert(`Olá ${nome}! Sejá bem-vindo!`);
+    document.getElementById('formularioLogin').style.display ='none';
+}
+
