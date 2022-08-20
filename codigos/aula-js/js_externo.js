e3762b3a38f20f148854f80cef27a6bcb3ff90b8
Renato Soares <renatosilsoa@gmail.com>
2022-08-20 10:02:20 -0300

    aula 01 js - inserir js interno e externo

---
+++ codigos/aula-js/js_externo.js
@@ -0,0 +1,6 @@
+document.querySelector("#titulo").textContent = "aula de javascript -Introdução";
+document.querySelector("#sub_titulo").textContent ="Bora pra cima"
+
+const  nome = prompt("Qual é o seu nome");
+
+alert (`Que bom ter você aqui ${nome}! \n Vamos Começar?`);
