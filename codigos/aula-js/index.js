3615c7ded9c9612b938e2c467beb59a97dfc9d68
Renato Soares <renatosilsoa@gmail.com>
2022-08-21 18:52:17 -0300

    aula 01 js - cont. adição da lista suspensa

--- codigos/aula-js/index.js
+++ codigos/aula-js/index.js
@@ -1,17 +1,17 @@
 const listaTarefas =document.querySelector("#listaTarefas");
-
 const caixaTexto = document.querySelector('#caixaDeTexto')
-
 const botaoAdiconar = document.querySelector("#btAdicionar")
+const listaSuspensa =document.querySelector("#listaSuspensa")
 
 botaoAdiconar.addEventListener("click", function(){
     const textoDaTarefa = caixaTexto.value;
     caixaTexto.value ='';
-
     listaTarefas.appendChild(adiconaTarefa(textoDaTarefa));
+    exibeOcultaListaSuspensa();
     caixaTexto.focus();
 });
 
+//botão ADICONAR TAREFA
 function adiconaTarefa(textoDaTarefa){
     const elementoLI = document.createElement('li');
     const elementoSPAN = document.createElement('span');
@@ -19,22 +19,49 @@ function adiconaTarefa(textoDaTarefa){
     elementoSPAN.setAttribute('class', 'tarefa');
     elementoSPAN.textContent = textoDaTarefa;
 
+    elementoLI.className ='naoRealizada';
     elementoLI.appendChild(elementoSPAN);
     elementoLI.appendChild(AdiconarBotaoRemover())
+
+
+    //LISTER - sempre que um item da lista for clicado pelo mouse
+    elementoSPAN.addEventListener("click", function(){
+        if(this.className === 'tarefa'){
+            if(this.parentNode.className ==='naoRealizada'){
+                this.parentNode.className = 'realizada'
+            }else{
+                this.parentNode.className = 'naoRealizada'
+                
+            };
+        }
+    });
 
     return elementoLI;
 }
 
+//BOTÃO REMOVER 
 function AdiconarBotaoRemover(){
     const botaoRemover =document.createElement('button');
 
     botaoRemover.textContent = "✘";
     botaoRemover.className ='remover';
-    console.log(listaTarefas)
 
+    //LISTENER -SEMPRE O BOTÃO REMOVER FOR CLICADO PELO MOUSE REMOVE O ITEM DA LISTA
     botaoRemover.addEventListener('click', function(){
-        listaTarefas.removeChild(this.parentNode);//pegar o nó pai do botão .
+        listaTarefas.removeChild(this.parentNode);
+        exibeOcultaListaSuspensa();
     })
 
     return botaoRemover;
+}
+
+function exibeOcultaListaSuspensa(){
+    const elementoSPAN = document.querySelector('.tarefa');
+    console.log(elementoSPAN)
+    if(elementoSPAN === null){
+        listaSuspensa.setAttribute('hidden', 'hidden');
+    }else{
+        listaSuspensa.removeAttribute('hidden');
+    }
+
 }
