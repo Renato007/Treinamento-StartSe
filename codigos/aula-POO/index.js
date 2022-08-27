9ea3f7c2608fef4a9b866fd8ba0ee8fb85a42658
Renato Soares <renatosilsoa@gmail.com>
2022-08-27 15:16:35 -0300

    aula04 - Modo Estrito no JavaScript

---
+++ codigos/aula-POO/index.js
@@ -0,0 +1,24 @@
+//exemplo 01
+
+/* 'use strict'
+let nome='Ana';
+nume="Pedro"
+
+console.log(nome)
+console.log(nume) */
+
+//exemplo 02
+/* let nome='Ana';
+nome="Pedro"
+
+function  exibirNome (){
+    'use strict'
+    a = 30;
+    console.log(a);
+    console.log(nome);
+}
+exibirNome(nome); */
+
+/* Utilização do modo Estrito
+Banstante útil para identificar erros de nomes de variáveis não declaradas 
+*/
