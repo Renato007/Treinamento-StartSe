3d9b090325c8bc12bdee8b511be410e6660813fb
Renato Soares <renatosilsoa@gmail.com>
2022-08-28 19:40:16 -0300

    subtração do valor com o valor de entrada

---
+++ codigos/simuladorDeFinanciamento_Desafio/parcela.js
@@ -0,0 +1,16 @@
+export class Parcela{
+    #numero;
+    #valor;
+    #juros;
+    #amortizacao;
+    #saldo;
+
+    constructor(numero, valor, juros, amortizacao, saldo){
+        this.#numero =numero;
+        this.#valor =valor;
+        this.#juros =juros;
+        this.#amortizacao =amortizacao;
+        this.#saldo =saldo;
+    }
+    //não vai precisar acessar todos os valores com get
+}
