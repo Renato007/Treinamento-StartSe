3a17ccbb9deb68e51cc458a257af02b2cfea7b4b
Renato Soares <renatosilsoa@gmail.com>
2022-08-28 15:15:03 -0300

    modulárizando as classes

--- codigos/aula-POO/index.js
+++ codigos/aula-POO/index.js
@@ -3,16 +3,8 @@ import { Musica } from "./musica.js"
 import { Produto } from "./produto.js";
 const musica1 = new Musica('Minha música', 100);
 
-console.log(musica1.formato);
-console.log(Musica.formato);
-
-const produto01 = new Produto(20, 'Meu produto', 35, Produto.GRANDE)
-console.log(produto01);
-
-const prod =new Produto('Refrigerante', 10, 3, 2, 20);
-
-console.log(prod.volumeProduto())
-console.log(prod.parcelaValor(2))
+/* console.log(musica1.converteSegundosEmMinutos(musica1.getDuracao()));
+ */console.log(Musica.converteSegundosEmMinutos(musica1.getDuracao()));
 
 //Declarando Métodos de uma Classe
 /* 
--- codigos/aula-POO/musica.js
+++ codigos/aula-POO/musica.js
@@ -7,4 +7,17 @@ export class Musica{
         this.titulo = titulo;
         this.duracao = duracao;
     }
+    static converteSegundosEmMinutos(duracao){
+        const min = Math.floor(duracao / 60);
+        let seg = duracao % 60;
+        if (seg < 10 && seg > 0){
+            seg = `0${duracao % 60}`;
+        }else if (seg < 10){
+            seg =`00`;
+        }
+        return `${min}:${seg}`;
+    }
+    getDuracao(){
+        return this.duracao;
+    }
 }
